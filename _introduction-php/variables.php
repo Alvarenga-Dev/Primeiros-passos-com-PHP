282415d1d8fa8f8fa4f0674423edb3d161c40866
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2019-12-30 19:26:15 -0300

    Alteração no nome das pastas!

--- _introduction-php/variables.php
+++ _introduction-php/variables.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Funções no PHP: Variáveis - Lucas Alvarenga</title>
+    <title>Introdução ao PHP: Variáveis - Lucas Alvarenga</title>
     <!-- CSS -->
     <link rel="stylesheet" href="../css/style.css">
 </head>
